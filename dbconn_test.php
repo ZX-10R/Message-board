345212c65ab5e917d968c8ce11336a659d7366e3
kuma rin <vallierekuma@gmail.com>
2021-12-23 12:53:30 +0900

    require file path changes

--- dbconn_test.php
+++ dbconn_test.php
@@ -1,7 +1,7 @@
 <?php
     $errcode = "";
     try{
-        require_once('./gitignore/DBinfo.php');
+        require_once('./ignorefiles/DBinfo.php');
 
         $pdo = new PDO(DBinfo::DSN, DBinfo::USER, DBinfo::PASSWORD);
         $errmessage = "connected";
