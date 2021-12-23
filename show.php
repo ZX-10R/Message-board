b54b8fd0939881832c5e0d15c8ffe40692a3229a
kuma rin <vallierekuma@gmail.com>
2021-12-23 13:26:05 +0900

    Call to a member function query() on null

--- show.php
+++ show.php
@@ -4,22 +4,25 @@ try{
     require_once("./ignorefiles/DBinfo.php");
     $pdo = new PDO(DBinfo::DSN, DBinfo::USER, DBinfo::PASSWORD);
 
+
+    $sql = 'SELECT * FROM posts ORDER BY created DESC';
+    $statement = $pdo->query($sql);
+
+
+    if($row = $statement->fetch()){
+        print("{$row[0]},{$row[1]},{$row[2]},{$row[3]},{$row[4]}<br/>");
+    }
+    else{
+        print("no data.<br/>");
+    }
 }
+
 catch(PDOException $e){
     $errcode = $e -> getCode();
     $errmessage = $e -> getMessage();
     $err = $errcode + $errmessage;
 }
-$sql = 'SELECT * FROM posts ORDER BY created DESC';
-$statement = $pdo->query($sql);
 
-
-if($row = $statement->fetch()){
-	print("{$row[0]},{$row[1]},{$row[2]},{$row[3]},{$row[4]}<br/>");
-}
-else{
-	print("no data.<br/>");
-}
 
  $pdo = null;
 ?>
